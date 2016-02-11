e278c37a44db1207d5a85ed81bd0c09699d86dd3
Daniel Jackson <daniel@lakraven.com>
2016-02-11 21:40:19 +0000

    - Removed TestInsightSettings.ini because we don't need it - Defined the ADAPT Floating Point Type, `ADFloat`, in ADAPT.Common.Types.Base.pas

--- Source/Pascal/Lib/ADAPT.Common.Types.Base.pas
+++ Source/Pascal/Lib/ADAPT.Common.Types.Base.pas
@@ -39,7 +39,60 @@
 }
 unit ADAPT.Common.Types.Base;
 
+{$I ADAPT.inc}
+
+{$IFDEF FPC}
+  {$IFNDEF ADAPT_SUPPRESS_VERSION_WARNING}
+    {.$IF FPC_VERSION < 3}
+      {.$ERROR 'FreePascal (FPC) 3.0 or above is required for the ADAPT.'}
+      {.$DEFINE ADAPT_WARNING_VERSION}
+    {.$IFEND FPC_VERSION}
+  {$ENDIF ADAPT_SUPPRESS_VERSION_WARNING}
+{$ELSE}
+  {$IFNDEF ADAPT_SUPPRESS_VERSION_WARNING}
+    {$IFNDEF DELPHIXE2_UP}
+      {$MESSAGE WARN 'Delphi 2010 and XE are not regularly tested with the ADAPT. Please report any issues on https://github.com/LaKraven/ADAPT'}
+      {$DEFINE ADAPT_WARNING_VERSION}
+    {$ENDIF DELPHIXE2_UP}
+  {$ENDIF ADAPT_SUPPRESS_VERSION_WARNING}
+{$ENDIF FPC}
+
+{$IFDEF ADAPT_WARNING_VERSION}
+  {$MESSAGE HINT 'Define "ADAPT_SUPPRESS_VERSION_WARNING" in your project options to get rid of these messages'}
+  {$UNDEF ADAPT_WARNING_VERSION}
+{$ENDIF ADAPT_WARNING_VERSION}
+
+{$IFNDEF ADAPT_SUPPRESS_DEPRECATION_WARNING}
+  // Nothing deprecated to warn about at this moment
+  {$IFDEF ADAPT_WARNING_DEPRECATION}
+    {$MESSAGE HINT 'Define "ADAPT_SUPPRESS_DEPRECATION_WARNING" in your project options to get rid of these messages'}
+  {$ENDIF ADAPT_WARNING_DEPRECATION}
+{$ENDIF ADAPT_SUPPRESS_DEPRECATION_WARNING}
+
 interface
+
+uses
+  {$IFDEF ADAPT_USE_EXPLICIT_UNIT_NAMES}
+    System.Classes, System.SysUtils, System.SyncObjs;
+  {$ELSE}
+    Classes, SysUtils, SyncObjs;
+  {$ENDIF ADAPT_USE_EXPLICIT_UNIT_NAMES}
+
+  {$I ADAPT_RTTI.inc}
+
+type
+  {$IFDEF ADAPT_FLOAT_SINGLE}
+    ///  <summary><c>Single-Precision Floating Point Type.</c></summary>
+    ADFloat = Single;
+  {$ELSE}
+    {$IFDEF ADAPT_FLOAT_EXTENDED}
+      ///  <summary><c>Extended-Precision Floating Point Type.</c></summary>
+      ADFloat = Extended;
+    {$ELSE}
+      ///  <summary><c>Double-Precision Floating Point Type.</c></summary>
+      ADFloat = Double; // This is our default
+    {$ENDIF ADAPT_FLOAT_DOUBLE}
+  {$ENDIF ADAPT_FLOAT_SINGLE}
 
 implementation
 
