2a9bceef6aec395b317f2808408c76f76b8c52d6
Daniel Jackson <daniel@lakraven.com>
2016-02-11 20:35:16 +0000

    Initial INSTALL batch file added

---
+++ INSTALL.bat
@@ -0,0 +1,23 @@
+@ECHO OFF
+cls
+echo A.D.A.P.T.
+echo --------------------
+echo (A)dvanced
+echo (D)eveloper
+echo (A)sync
+echo (P)rogramming
+echo (T)oolkit
+echo.
+echo Setting Environment Variables for ADAPT Paths
+
+echo Registering ADAPT_HOME
+setx ADAPT_HOME %~dp0
+echo registering ADAPT_PASCAL
+setx ADAPT_PASCAL %~dp0Source\Pascal\Lib
+echo ADAPT Paths added...
+echo -------------------
+echo The following Environment Variables have been registered:
+echo ADAPT_HOME		Points to the ADAPT Root Directory
+echo ADAPT_PASCAL		Points to $(ADAPT_LIB)\Pascal
+
+pause
