95dab2a59bc64bf8d7751b718c9be7e9c5b10c9c
Daniel Jackson <daniel@lakraven.com>
2016-07-23 14:05:28 +0100

    - Introduced textually-correct (Markdown Formatting unverified) LICENSE.md file - Shortened the length of the Copyright Comment Block in all Library units.

--- Source/Pascal/Lib/ADAPT.Streams.Threadsafe.pas
+++ Source/Pascal/Lib/ADAPT.Streams.Threadsafe.pas
@@ -3,39 +3,7 @@
   Copyright (C) 2014-2016, Simon J Stuart, All Rights Reserved
 
   Original Source Location: https://github.com/LaKraven/ADAPT
-
-  Formerlly known as "LaKraven Studios Standard Library" or "LKSL".
-  "ADAPT" supercedes the former LKSL codebase as of 2016.
-
-  License:
-    - You may use this library as you see fit, including use within commercial applications.
-    - You may modify this library to suit your needs, without the requirement of distributing
-      modified versions.
-    - You may redistribute this library (in part or whole) individually, or as part of any
-      other works.
-    - You must NOT charge a fee for the distribution of this library (compiled or in its
-      source form). It MUST be distributed freely.
-    - This license and the surrounding comment block MUST remain in place on all copies and
-      modified versions of this source code.
-    - Modified versions of this source MUST be clearly marked, including the name of the
-      person(s) and/or organization(s) responsible for the changes, and a SEPARATE "changelog"
-      detailing all additions/deletions/modifications made.
-
-  Disclaimer:
-    - Your use of this source constitutes your understanding and acceptance of this
-      disclaimer.
-    - Simon J Stuart, nor any other contributor, may be held liable for your use of this source
-      code. This includes any losses and/or damages resulting from your use of this source
-      code, be they physical, financial, or psychological.
-    - There is no warranty or guarantee (implicit or otherwise) provided with this source
-      code. It is provided on an "AS-IS" basis.
-
-  Donations:
-    - While not mandatory, contributions are always appreciated. They help keep the coffee
-      flowing during the long hours invested in this and all other Open Source projects we
-      produce.
-    - Donations can be made via PayPal to PayPal [at] LaKraven (dot) Com
-                                          ^  Garbled to prevent spam!  ^
+  Subject to original License: https://github.com/LaKraven/ADAPT/blob/master/LICENSE.md
 }
 unit ADAPT.Streams.Threadsafe;
 
