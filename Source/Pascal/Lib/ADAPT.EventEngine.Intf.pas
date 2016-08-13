56fa9be2b3ddcefff7d051b7ac7306ec1df7ab90
Daniel Jackson <daniel@lakraven.com>
2016-08-13 12:46:46 +0100

    - Defined `IADEventListener<T: IADEvent>` (Issue #90)

--- Source/Pascal/Lib/ADAPT.EventEngine.Intf.pas
+++ Source/Pascal/Lib/ADAPT.EventEngine.Intf.pas
@@ -26,6 +26,9 @@ uses
   {$I ADAPT_RTTI.inc}
 
 type
+  { Forward Declarations }
+  IADEventThread = interface;
+
   { Enums }
   TADEventDispatchMethod = (dmQueue, dmStack);
   TADEventDispatchTarget = (dtThreads);
@@ -35,10 +38,17 @@ type
   { Sets }
   TADEventDispatchTargets = set of TADEventDispatchTarget;
 
+  ///  <summary><c>Fundamental Interface for all Event Types.</c></summary>
+  ///  <remarks>
+  ///    <para><c>Remember that Events do NOT provide functionality, only information.</c></para>
+  ///  </remarks>
   IADEvent = interface(IADInterface)
+  ['{CAFF660F-37F4-49B3-BBCC-15096C4B8E50}']
     // Getters
     ///  <returns><c>The Time at which the Event was Created.</c></returns>
     function GetCreatedTime: ADFloat;
+    ///  <returns><c>The Time Differential (Delta) between Dispatch and Now.</c></returns>
+    function GetDelta: ADFloat;
     ///  <returns><c>The Time (in Seconds) after which the Event should be Dispatched.</c></returns>
     ///  <remarks>
     ///    <para><c>Value represents an Offset (in Seconds) from DispatchTime</c></para>
@@ -68,11 +78,24 @@ type
     function GetProcessedTime: ADFloat;
     ///  <returns><c>Current State of this Event.</c></returns>
     function GetState: TADEventState;
+
     // Setters
+
+    // Management Methods
+    ///  <summary><c>Dispatches the Event through the Event Engine Queue.</c></summary>
+    procedure Queue;
+    ///  <summary><c>Schedules the Event for Dispatch through the Event Engine Queue.</c></summary>
+    procedure QueueSchedule(const AScheduleFor: ADFloat);
+    ///  <summary><c>Dispatches the Event through the Event Engine Stack.</c></summary>
+    procedure Stack;
+    ///  <summary><c>Schedules the Event for Dispatch through the Event Engine Stack.</c></summary>
+    procedure StackSchedule(const AScheduleFor: ADFloat);
 
     // Properties
     ///  <summary><c>The Time at which the Event was Created.</c></summary>
     property CreatedTime: ADFloat read GetCreatedTime;
+    ///  <returns><c>The Time Differential (Delta) between Dispatch and Now.</c></returns>
+    property Delta: ADFloat read GetDelta;
     ///  <returns><c>The Time (in Seconds) after which the Event should be Dispatched.</c></returns>
     ///  <remarks>
     ///    <para><c>Value represents an Offset (in Seconds) from DispatchTime</c></para>
@@ -102,6 +125,59 @@ type
     property ProcessedTime: ADFloat read GetProcessedTime;
     ///  <returns><c>Current State of this Event.</c></returns>
     property State: TADEventState read GetState;
+  end;
+
+  ///  <summary><c>Event Listeners are invoked when their relevent Event Type is processed through the Event Engine.</c></summary>
+  IADEventListener<T: IADEvent> = interface(IADInterface)
+    // Getters
+    ///  <returns><c>A reference to the Event Thread owning this Listener.</c></returns>
+    ///  <remarks>
+    ///    <para><c>Could be </c>nil<c> if the Listener has no owning Event Thread.</c></para>
+    ///  </remarks>
+    function GetEventThread: IADEventThread;
+    ///  <returns><c>The Reference Time of the last processed Event.</c></returns>
+    function GetLastProcessed: ADFloat;
+    ///  <returns>The Maximum Age (Delta) of an Event before this Listener will no longer process it.</c></returns>
+    function GetMaxAge: ADFloat;
+    ///  <returns>
+    ///    <para>True<c> if this Listener will only process an Event if it is newer than the last Processed Event.</c></para>
+    ///    <para>False<c> if this Listener doesn't care whether an Event is newer or older than the last Processed Event.</c></para>
+    ///  </returns>
+    function GetNewestOnly: Boolean;
+
+    // Setters
+    ///  <summary><c>Defines the Maximum Age (Delta) of an Event before this Listener will no longer process it.</c></summary>
+    procedure SetMaxAge(const AMaxAge: ADFloat);
+    ///  <summary><c>Defines whether or not this Listener will only process an Event if it is newer than the last Processed Event.</c></summary>
+    procedure SetNewestOnly(const ANewestOnly: Boolean);
+
+    // Management Methods
+    ///  <summary><c>Registers the Event Listner so that it can begin processing Events.</c></summary>
+    procedure Register;
+    ///  <summary><c>Unregisters the Event Listener. It will no longer respond to Events.</c></summary>
+    procedure Unregister;
+
+    // Properties
+    ///  <returns><c>A reference to the Event Thread owning this Listener.</c></returns>
+    ///  <remarks>
+    ///    <para><c>Could be </c>nil<c> if the Listener has no owning Event Thread.</c></para>
+    ///  </remarks>
+    property EventThread: IADEventThread read GetEventThread;
+    ///  <summary><c>Defines the Maximum Age (Delta) of an Event before this Listener will no longer process it.</c></summary>
+    ///  <returns>The Maximum Age (Delta) of an Event before this Listener will no longer process it.</c></returns>
+    property MaxAge: ADFloat read GetMaxAge write SetMaxAge;
+    ///  <summary><c>Defines whether or not this Listener will only process an Event if it is newer than the last Processed Event.</c></summary>
+    ///  <returns>
+    ///    <para>True<c> if this Listener will only process an Event if it is newer than the last Processed Event.</c></para>
+    ///    <para>False<c> if this Listener doesn't care whether an Event is newer or older than the last Processed Event.</c></para>
+    ///  </returns>
+    property NewestOnly: Boolean read GetNewestOnly write SetNewestOnly;
+  end;
+
+  ///  <summary><c>Event Threads are specialized "Precision Threads" designed to process Asynchronous Events.</c></summary>
+  IADEventThread = interface(IADInterface)
+  ['{0374AD53-2EB7-45BB-8402-1FD34D40512B}']
+
   end;
 
 implementation
